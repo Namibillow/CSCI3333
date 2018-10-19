4f6a449109bc5bd47b99b83ac3349f9680d1ac82
Nami <akazawan73@gmail.com>
2018-10-19 00:20:22 -0500

    fixed

--- HWK7/mine.cpp
+++ HWK7/mine.cpp
@@ -6,4 +6,41 @@
 //  Copyright © 2018 Nami. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include "mine.h"
+#include "batcoin.h"
+
+BlockHeader mine(BlockHeader prev, int difficulty){
+    BlockHeader b;
+    bathash(prev, b.prev);
+
+    for(int i=0; i< 8; i++){
+        b.nonce[i] =  '0' + (rand() % 255);
+    }
+    bool zeros;
+    while(true){
+        zeros = false;
+        BlockHeader temp;
+        temp = b;
+        bathash(b, temp.prev);
+        
+        for(int j=0; j< difficulty; j++){
+            if(temp.prev[j]== 0){
+                zeros = true;
+            }
+            else{
+                zeros = false;
+                break;
+            }
+        }
+        if(zeros){
+            return b;
+        }
+        else{
+            for(int i=0; i< 8; i++){
+                b.nonce[i] =  '0' + (rand() % 255);
+            }
+        }
+    }
+}
